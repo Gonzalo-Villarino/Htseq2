3b1b299945de64f773238a8b2ddfe397a338b514
Gonzalo Villarino <ghv8@cornell.edu>
2016-02-23 16:32:14 -0500

    Update generate_tophat.pl

--- generate_tophat.pl
+++ generate_tophat.pl
@@ -8,7 +8,7 @@ while(<FILE>){
 	$name =~ s/\.Q30L50\.fq\.gz//;
 	open(OUT,">$name.sh") or die;
 	print OUT "#!/bin/bash\n";
-	print OUT "tophat2 -G /home3/rnaseq-shared/Gonzalo/genes_gtf/Arabidopsis_thaliana.TAIR10.26.gtf_final -I 2000 -o /home3/rnaseq-shared/Gonzalo/01_tophat/test_perl/tophat_$name --library-type=fr-firststrand /home3/rnaseq-shared/Gonzalo/indexed_genome/genome /home3/rnaseq-shared/Gonzalo/00_cleaned_reads/$name.Q30L50.fq.gz";
+	print OUT "tophat2 -G /home3/genes_gtf/Arabidopsis_thaliana.TAIR10.26.gtf_final -I 2000 -o /home3/Gonzalo/01_tophat/test_perl/tophat_$name --library-type=fr-firststrand /home3/rnaseq-shared/Gonzalo/indexed_genome/genome /home3/rnaseq-shared/Gonzalo/00_cleaned_reads/$name.Q30L50.fq.gz";
 	print $name;
 	close OUT;
 }
